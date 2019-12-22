e4508d0292f1bdbd7e73be6a54b8a3ef2d80ace5
Dalibor Sojic <sojic@livenetworks.mk>
2019-12-22 01:17:58 +0100

    Initial commit

---
+++ LNImages/Image.php
@@ -0,0 +1,104 @@
+<?php
+
+class Image {
+	public $dirname;
+	public $basename;
+	public $ext;
+	public $filename;
+	public $source;
+
+	private $_allowedExt = [
+		'webp',
+		'png',
+		'jpg'
+	];
+
+
+	public function __construct() {
+		return $this;
+	}
+
+
+	public function setSourceDir($sourceDir) {
+		$this->_sourceDir = realpath($sourceDir);
+		return $this;
+	}
+
+	public function setTargetDir($targetDir) {
+		$this->_targetDir = $targetDir;
+		return $this;
+	}
+	
+
+	private function _parseSize() {
+		preg_match('/-w(\d+)/', $this->filename, $r);
+		$this->_size->width = (isset($r[1])) ? $r[1] : -1;
+
+		preg_match('/-h(\d+)/', $this->filename, $r);
+		$this->_size->height = (isset($r[1])) ? $r[1] : -1;
+
+		return $this;
+	}
+	
+	private function _findSource() {
+		$this->_sourceFile = $this->_sourceDir . $this->dirname . DIRECTORY_SEPARATOR . $this->filename;
+
+
+		$this->_sourceFile = preg_replace('/-w(\d+)/', '', $this->_sourceFile);
+		$this->_sourceFile = preg_replace('/-h(\d+)/', '', $this->_sourceFile);
+
+		// Original, source, path, without extension
+
+		foreach($this->_allowedExt as $ext) {
+			$file = $this->_sourceFile . '.' . $ext;
+			if (is_file($file)) {
+				$this->_sourceFile = $file;
+				return $this;
+			}
+		}
+		return $this;
+	}
+
+	public function _prepareTarget() {
+		$target = realpath($this->_targetDir) . DIRECTORY_SEPARATOR . $this->dirname;
+		if (!is_dir($target)) {
+			echo "Create Dir " . $target;
+			mkdir($target, 0755, true);
+		}
+		$this->targetFile = realpath($target) . DIRECTORY_SEPARATOR . $this->filename . '.' . $this->ext;
+		return $this;
+	}
+
+	public function _execute() {
+		// ffmpeg -i amazing-80s.webp -vf scale=200:400 x.png
+
+		$cmd = '/usr/bin/ffmpeg ';
+		$params[] = '-i ' . $this->_sourceFile;
+		$params[] = '-vf scale=' . $this->_size->width . ':' . $this->_size->height;
+		$params[] = $this->targetFile;
+
+		exec($cmd . join(' ', $params));
+
+	}
+
+
+	public function create($image) {
+
+		$this->targetFile = $image;
+		$pi = pathinfo($image);
+
+		$this->dirname	= $pi['dirname'];
+		$this->basename	= $pi['basename'];
+		$this->ext		= $pi['extension'];
+		$this->filename	= $pi['filename'];
+		unset($pi);
+
+		print_r($this);
+
+		$this->_size = new StdClass();
+		$this->_parseSize()
+				->_findSource()
+				->_prepareTarget()
+				->_execute();
+	}
+}
