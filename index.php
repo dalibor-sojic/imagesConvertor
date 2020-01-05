b4af1742fd478b995c58a86cf3b7254d73a99413
Dalibor Sojic <sojic@livenetworks.mk>
2020-01-05 20:18:04 +0100

    Update

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
 include('LNImages/Image.php');
 
+$requestedFile = (isset($_GET['file'])) ? $_GET['file'] : $argv[1];
+
 $image = new Image();
 $image->setSourceDir('/home/console/files/amazingradios/files/cdnsources/');
 $image->setTargetDir('/home/console/files/amazingradios/files/cdn/');
 
-$image->create($_GET['file']);
+$image->create($requestedFile);
 
 exec('/usr/bin/php /home/console/nextcloud/occ files:scan amazingradios > /dev/null &');
 
-header('Location: ' . $_GET['file']);
+header('Location: ' . $requestedFile);
